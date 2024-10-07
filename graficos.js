20cae85b757e59c6e702290541b2069815dba763
Daniely1103 <daniely.correia@escola.pr.gov.br>
2024-10-07 11:46:28 -0300

    Add files via upload

--- graficos.js
+++ graficos.js
@@ -5,4 +5,25 @@ conts graficoPizza = new Chart(ctxPizza, {
     data: {
         labels:['Motorola','Samsung','Iphone','Xiaomi'],
         datasets: [{
-            label:
+            label:''
+            data:
+            backgrondColor: [
+                '#F05454', '#30475E' , '#DDDDDD' , '#222831'
+                ],
+                borderColor: '#DDDDDD' ,
+                borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'bottom',
+                        },
+                        title: {
+                            display: true,
+                            text: ''
+                            }
+                        }
+                    }
+                });
