a27e9a0a83714432d8203393eb629ad42b4dae58
Daniely1103 <daniely.correia@escola.pr.gov.br>
2024-10-22 09:08:11 -0300

    Add files via upload

--- graficoo-main/graficos.js
+++ graficoo-main/graficos.js
@@ -5,8 +5,124 @@ conts graficoPizza = new Chart(ctxPizza, {
     data: {
         labels:['Motorola','Samsung','Iphone','Xiaomi'],
         datasets: [{
-            label:''
-            data:
+            label:'Qual a marca do seu celular?'
+            data: [20, 40, 0, 60]
+            backgrondColor: [
+                '#F05454', '#30475E' , '#DDDDDD' , '#222831'
+                ],
+                borderColor: '#DDDDDD' ,
+                borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'bottom',
+                        },
+                        title: {
+                            display: true,
+                            text: ''
+                            }
+                        }
+                    }
+                });
+//Grafico de Pizza
+const ctxPizza = document.getElementById('grafico pizza').gerContext('2d');
+conts graficoPizza = new Chart(ctxPizza, {
+    type: 'pie' ,
+    data: {
+        labels:['1-2 meses','3-6 meses','6-12 meses','1-2 anos' ,'mais de 2 anos'],
+        datasets: [{
+            label:'Ha quanto tempo você possui seu celular?'
+            data: [7,1; 14,3; 14,3; 42,9; 21,4],
+            backgrondColor: [
+                '#F05454', '#30475E' , '#DDDDDD' , '#222831'
+                ],
+                borderColor: '#DDDDDD' ,
+                borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'bottom',
+                        },
+                        title: {
+                            display: true,
+                            text: ''
+                            }
+                        }
+                    }
+                });
+                //Grafico de Pizza
+const ctxPizza = document.getElementById('grafico pizza').gerContext('2d');
+conts graficoPizza = new Chart(ctxPizza, {
+    type: 'pie' ,
+    data: {
+        labels:['1','2','3','mais de 3 vezes'],
+        datasets: [{
+            label:'Quantas vezes ao dia você carrega seu celular?'
+            data: [50, 35,7; 7,1; 7,1;],
+            backgrondColor: [
+                '#F05454', '#30475E' , '#DDDDDD' , '#222831'
+                ],
+                borderColor: '#DDDDDD' ,
+                borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'bottom',
+                        },
+                        title: {
+                            display: true,
+                            text: ''
+                            }
+                        }
+                    }
+                });
+                //Grafico de Pizza
+const ctxPizza = document.getElementById('grafico pizza').gerContext('2d');
+conts graficoPizza = new Chart(ctxPizza, {
+    type: 'pie' ,
+    data: {
+        labels:['1-2 horas','3-6 horas','6-12 horas','12-24 horas'],
+        datasets: [{
+            label:'Quantas horas por dia você usa seu celular?'
+            data: [0, 50, 28,6; 21,4],
+            backgrondColor: [
+                '#F05454', '#30475E' , '#DDDDDD' , '#222831'
+                ],
+                borderColor: '#DDDDDD' ,
+                borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'bottom',
+                        },
+                        title: {
+                            display: true,
+                            text: ''
+                            }
+                        }
+                    }
+                });
+                //Grafico de Pizza
+const ctxPizza = document.getElementById('grafico pizza').gerContext('2d');
+conts graficoPizza = new Chart(ctxPizza, {
+    type: 'pie' ,
+    data: {
+        labels:['Instagram','Whatsapp','Facebook','Tik Tok','Twitter'],
+        datasets: [{
+            label:'Qual rede social você mais utiliza?'
+            data: [50,20,10,10,10]
             backgrondColor: [
                 '#F05454', '#30475E' , '#DDDDDD' , '#222831'
                 ],
